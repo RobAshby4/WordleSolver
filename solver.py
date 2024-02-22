816bc126c80f3e42de5eb54878d22bcb7bb59c92
RobAshby4 <robert.n.ashby@gmail.com>
2024-02-22 01:14:56 -0800

    Generates a whole board :)

--- solver.py
+++ solver.py
@@ -19,9 +19,16 @@ class Letter:
     def set_character(character):
         self.character = character
 
-def create_row(element):
-    row = []     
-    print(element)
+class Row:
+    word = None
+    letters = []
+    def __init__(self, row):
+        row_word = ""
+        elements = row.find_elements(By.XPATH, './div/*')
+        for element in elements:
+            row_word = row_word + element.text
+            self.letters.append(Letter(element.get_attribute('data-state'), element.text))
+        self.word = row_word
 
 class Board:
     rows = {
@@ -35,8 +42,11 @@ class Board:
 
     def __init__(self, board_elem, guess_num):
         for i in range (1, 7):
-            next_element = driver.find_element(By.CSS_SELECTOR, '[aria-label="Row {}"]'.format(i))
-            self.rows[i] = create_row(next_element)
+            next_row = driver.find_element(By.XPATH, '//div[@aria-label="Row {}"]'.format(i))
+            self.rows[i] = Row(next_row)
+            print(str(i))
+            print(self.rows[i].word)
+
 
 def get_current_board():
     global driver 
@@ -51,12 +61,12 @@ def main():
     driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/div[2]/button[2]").click()
     time.sleep(1)
     driver.find_element(By.XPATH, "/html/body/div/div/dialog/div/div/button").click()
-    current_board = get_current_board()
     body = driver.find_element(By.XPATH, "html/body")
     time.sleep(1)
     body.send_keys("arise", Keys.RETURN)
-    time.sleep(5)
+    time.sleep(3)
     body.send_keys("match", Keys.RETURN)
+    current_board = get_current_board()
     time.sleep(100)
 
 if __name__ == "__main__":
